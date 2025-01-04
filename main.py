481dd5f069d5621f2ae04271617aee37fbb8a572
Sjassim09 <jassimsirat@gmail.com>
2025-01-04 16:53:44 +0100

    placement des bateaux ( pouvoir les tourner )

--- main.py
+++ main.py
@@ -86,46 +86,35 @@ class Ordinateur(Joueur):
             navire = Navire(nom, taille)
             self.plateau.placement_aleatoire(navire)
 
-# Interface graphique
 class InterfaceBatailleNavale:
     def __init__(self, root):
         self.root = root
         self.root.title("Bataille Navale")
-        self.joueur = Joueur("Joueur")
-        self.ordinateur = Ordinateur()
-        self.navires_a_placer = [("Porte-avions", 5), ("Croiseur", 4), ("Destroyer", 3), ("Sous-marin", 2)]
-        self.navire_en_cours = None
-        self.creer_interface()
-    # Fonction pour créer l'interface graphique
-    def creer_interface(self):
-        frame = tk.Frame(self.root)
-        frame.pack()
-        
-        self.label_tour = tk.Label(frame, text="Placement des navires", font=("Arial", 12))
+        self.label_tour = tk.Label(root, text="Placement des navires")
         self.label_tour.grid(row=0, column=0, columnspan=10)
         
-        self.grille_joueur = [[tk.Button(frame, width=3, height=1, command=lambda x=i, y=j: self.placer_navire(x, y)) for j in range(10)] for i in range(10)]
+        self.frame_joueur = tk.Frame(root)
+        self.frame_joueur.grid(row=1, column=0, columnspan=10)
+        
+        self.frame_ordinateur = tk.Frame(root)
+        self.frame_ordinateur.grid(row=1, column=0, columnspan=10)
+        self.frame_ordinateur.grid_forget()
+        
+        self.grille_joueur = [[tk.Button(self.frame_joueur, width=2, height=1, command=lambda x=i, y=j: self.placer_navire_joueur(x, y)) for j in range(10)] for i in range(10)]
+        self.grille_ordinateur = [[tk.Button(self.frame_ordinateur, width=2, height=1, command=lambda x=i, y=j: self.tirer(x, y)) for j in range(10)] for i in range(10)]
+        
         for i in range(10):
             for j in range(10):
-                self.grille_joueur[i][j].grid(row=i+1, column=j)
+                self.grille_joueur[i][j].grid(row=i, column=j)
         
-        self.bouton_nouvelle_partie = tk.Button(self.root, text="Nouvelle Partie", command=self.nouvelle_partie)
-        self.bouton_nouvelle_partie.pack()
-    # Fonction pour placer un navire sur la grille
-    def placer_navire(self, x, y):
-        if self.navires_a_placer:
-            nom, taille = self.navires_a_placer[0]
-            self.navire_en_cours = Navire(nom, taille)
-            positions = [(x, y + i) for i in range(taille) if y + i < 10]
-            if len(positions) == taille and all(self.joueur.plateau.grille[x][y] == ' ' for x, y in positions):
-                self.joueur.plateau.placer_navire(self.navire_en_cours, positions)
-                for px, py in positions:
-                    self.grille_joueur[px][py].config(bg="gray")
-                self.navires_a_placer.pop(0)
-            if not self.navires_a_placer:
-                self.label_tour.config(text="Début de la partie !")
+        self.bouton_commencer = tk.Button(root, text="Commencer le jeu", command=self.afficher_plateau_jeu, state="disabled")
+        self.bouton_commencer.grid(row=2, column=0, columnspan=10)
+        
+        self.orientation = "H"  # H pour horizontal, V pour vertical
+        self.root.bind("<space>", self.changer_orientation)
+        
+        self.nouvelle_partie()
 
-    # Fonction pour démarrer une nouvelle partie
     def nouvelle_partie(self):
         print("Nouvelle partie démarrée")
         self.label_tour.config(text="Placement des navires")
@@ -135,6 +124,49 @@ class InterfaceBatailleNavale:
         for i in range(10):
             for j in range(10):
                 self.grille_joueur[i][j].config(bg="SystemButtonFace")
+        self.placer_navires_joueur()
+
+    def changer_orientation(self, event):
+        self.orientation = "V" if self.orientation == "H" else "H"
+        self.label_tour.config(text=f"Orientation: {'Horizontale' if self.orientation == 'H' else 'Verticale'}")
+
+    def placer_navires_joueur(self):
+        if self.navires_a_placer:
+            nom, taille = self.navires_a_placer[0]
+            self.navire_en_cours = Navire(nom, taille)
+            self.label_tour.config(text=f"Placez votre {nom} (taille {taille}) - Orientation: {'Horizontale' if self.orientation == 'H' else 'Verticale'}")
+        else:
+            self.label_tour.config(text="Tous les navires sont placés !")
+            self.bouton_commencer.config(state="normal")
+
+    def placer_navire_joueur(self, x, y):
+        if self.navire_en_cours:
+            if self.orientation == "H":
+                positions = [(x, y + i) for i in range(self.navire_en_cours.taille)]
+            else:
+                positions = [(x + i, y) for i in range(self.navire_en_cours.taille)]
+            
+            if all(0 <= pos[0] < 10 and 0 <= pos[1] < 10 and self.grille_joueur[pos[0]][pos[1]].cget('bg') == 'SystemButtonFace' for pos in positions):
+                for pos in positions:
+                    self.grille_joueur[pos[0]][pos[1]].config(bg='blue')
+                self.joueur.plateau.placer_navire(self.navire_en_cours, positions)
+                self.navires_a_placer.pop(0)
+                self.placer_navires_joueur()
+
+    def afficher_plateau_jeu(self):
+        self.frame_joueur.grid_forget()
+        self.frame_ordinateur.grid(row=1, column=0, columnspan=10)
+        self.label_tour.config(text="À vous de jouer !")
+
+    def tirer(self, x, y):
+        resultat = self.ordinateur.plateau.verifier_tir(x, y)
+        self.grille_ordinateur[x][y].config(text=resultat[0], state="disabled")
+        if resultat == "Coulé":
+            self.label_tour.config(text="Navire coulé !")
+        elif resultat == "Touché":
+            self.label_tour.config(text="Navire touché !")
+        else:
+            self.label_tour.config(text="Manqué !")
 
 # Fonction principale
 if __name__ == "__main__":
